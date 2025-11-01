3f3dd3536ffd1be3c148cd3cedb07ec11e05061c
장성원 <jjang6251@gmail.com>
2025-11-01 19:10:01 +0900

    test: 당첨 결과에 대한 총상금 반환 테스트 케이스 추가

--- src/test/java/lotto/LottosRankResultTest.java
+++ src/test/java/lotto/LottosRankResultTest.java
@@ -13,9 +13,9 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class LottosRankResultTest {
-    @DisplayName("랜덤 로또 번호에 대한 당첨 결과가 나온다")
+    @DisplayName("로또들에 대한 당첨 결과가 나온다")
     @Test
-    void 랜덤_로또_번호에_대한_당첨_결과가_나온다() {
+    void 로또들에_대한_당첨_결과가_나온다() {
         Lotto miss = LottoFactory.make(List.of(1, 2, 3, 4, 5, 6));
         Lotto rankFirst = LottoFactory.make(List.of(43, 17, 5, 2, 13, 19));
         Lotto rankSecond = LottoFactory.make(List.of(43, 17, 5, 2, 3, 19));
@@ -33,5 +33,22 @@ public class LottosRankResultTest {
         assertThat(lottosRankResult.countOf(Rank.FOURTH)).isEqualTo(0);
         assertThat(lottosRankResult.countOf(Rank.FIFTH)).isEqualTo(0);
         assertThat(lottosRankResult.countOf(Rank.MISS)).isEqualTo(1);
+    }
+
+    @DisplayName("로또들에 대한 총상금이 반환된다.")
+    @Test
+    void 로또들에_대한_총상금이_반환된다() {
+        Lotto miss = LottoFactory.make(List.of(1, 2, 3, 4, 5, 6));
+        Lotto rankFirst = LottoFactory.make(List.of(43, 17, 5, 2, 13, 19));
+        Lotto rankSecond = LottoFactory.make(List.of(43, 17, 5, 2, 3, 19));
+        Lotto rankThird = LottoFactory.make(List.of(43, 17, 5, 2, 12, 19));
+        Lotto winningNum = LottoFactory.make(List.of(43, 17, 5, 2, 13, 19));
+        Bonus bonus = new Bonus(3);
+        Lottos lottos = new Lottos(List.of(miss, rankFirst, rankSecond, rankThird));
+        WinningLotto winningLotto = new WinningLotto(winningNum, bonus);
+
+        LottosRankResult lottosRankResult = winningLotto.evaluate(lottos);
+
+        assertThat(lottosRankResult.returnTotalPrize()).isEqualTo(2_031_500_000);
     }
 }
